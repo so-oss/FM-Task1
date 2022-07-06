4066dce7f10d35b8b0382201b667186cdc5e2399
SoiriYuichi <sy@Soiri-MBA.local>
2022-07-06 22:03:49 +0900

    リストの作成

--- FM-Task1/FM-Task1/ViewController.swift
+++ FM-Task1/FM-Task1/ViewController.swift
@@ -7,31 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
-    var mytimer:Timer!
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
 
     @IBOutlet weak var countLabel: UILabel!
-    
     @IBOutlet weak var date: UILabel!
-    
     @IBOutlet weak var stockListTable: UITableView!
+    @IBOutlet weak var commentField: UITextField!
     
-    let stock = ["apple", "orange","melon"]
+    var mytimer:Timer!
+    var stock = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    
+
         timecheck()
-        
         mytimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timecheck), userInfo: nil, repeats: true)
-
-
+    
+        stockListTable.dataSource = self
+        stockListTable.delegate = self
+        commentField.delegate = self
+        
+    
     }
         
-    @IBAction func plusButton(_ sender: Any) {
-        
+    @IBAction func countUpButton(_ sender: Any) {
         var nowCount = Int(countLabel.text!)
         if nowCount! < 9999 {
             //+ボタンで１を足す
@@ -40,15 +39,19 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         }
     }
     
-    @IBAction func minusButton(_ sender: Any) {
-        
+    @IBAction func countDownButton(_ sender: Any) {
         var nowCount = Int(countLabel.text!)
         if nowCount! > 0 {
         //-ボタンで１を引く
         nowCount! -= 1
         countLabel.text = String.localizedStringWithFormat("%d", nowCount!)
         }
-        
+    }
+    
+    @IBAction func addStockList(_ sender: Any) {
+        stock.append(commentField.text!)
+        commentField.text = ""
+        stockListTable.reloadData()
     }
     
     @objc func timecheck(){
@@ -57,7 +60,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let formatter = DateFormatter()
         formatter.setLocalizedDateFormatFromTemplate("Hms")
         date.text = formatter.string(from: nowdate)
-    
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -67,8 +69,21 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "stockList", for: indexPath)
         cell.textLabel!.text = stock[indexPath.row]
+        
         return cell
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
 
+    
 }
 
